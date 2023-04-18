a1a7080d050d7755ec4029f4b54208e9fb59bebf
Elliott Tuan <gbs.tuan@gmail.com>
2023-04-19 01:14:38 +0800

    week-3 error fix

--- week-3/Node/index.js
+++ week-3/Node/index.js
@@ -4,36 +4,32 @@ const port = 3000
 
 app.set("view engine", "pug")
 
-//Assignment-2
-
 app.get('/', (req, res) => {
   res.send("Hello, My Server!")
 })
-// app.get('/:getData', (req, res) => {
-//     const getData = req.params.getData
-//     const number = req.query.number
-
-//     if(!number){
-//         res.send("Lack of Parameter")
-//     }else if(Number(number) > 0){ // && Number.isInterger(number)
-//         let total = (Number(number) +1) * Number(number) /2
-//         res.send("" + total)
-//     }else{
-//         res.send("Wrong Parameter")
-//     }
-// })
 
 //Assignment-3
 
-app.use(express.static("./public"))
-app.post("/sum", (req,res) => {
-    let number = req.query.number
-    const button = document.querySelector("#button")
-    button.addEventListener("click", (number) => {
-        (number+1)*number/2
-    })
-    res.send(number)
+app.use(express.static("public"))
+
+//Assignment-2
+
+app.get('/:getData', (req, res) => {
+    const getData = req.params.getData
+    const number = req.query.number
+    
+    if(!number){
+      res.send("Lack of Parameter")
+    }else if(isNaN(parseInt(number)) ){
+      res.send("Wrong Parameter")
+    }else{
+      let positiveInterger = parseInt(number)
+      let total = (positiveInterger+1)*positiveInterger/2
+      let output = total.toString()
+      res.send(output)
+    }
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
